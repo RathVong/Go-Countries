d2f26dfcd6f7a3bfa4c455c5bde7e8dc7888d8b5
Rath Vong <rathvong@mekongmessenger.com>
2017-07-30 10:28:13 -0400

    minor fixes

--- util.go
+++ util.go
@@ -6,17 +6,19 @@ import (
 )
 
 func ConvertStringToInt(s string) int {
-	if s == "" {
-		return 1
+
+	if len(s) == 0 {
+		return 0
 	}
 
 	c, err := strconv.ParseInt(s, 10, 64)
+	defer func() {
+		if err != nil {
+			log.Panicf("Country.ConvertStringToInt -> Error: ", err)
+		}
+	}()
 
 
-
-	defer func() { if err != nil {
-		log.Panicf("Country.ConvertStringToInt -> Error: ", err)
-	} }()
 
 	return int(c)
 
